8c24ef51b0c74ecc634eef9f452593ad3c7598cc
FuriousSpider <mario123gbm@gmail.com>
2020-03-19 15:24:36 +0100

    implement basic layout

---
+++ src/main/java/Util/Values.java
@@ -0,0 +1,6 @@
+package Util;
+
+public class Values {
+    public static int APPLICATION_MIN_WIDTH = 800;
+    public static int APPLICATION_MIN_HEIGHT = 600;
+}
--- src/main/java/simulator/Main.java
+++ src/main/java/simulator/Main.java
@@ -1,7 +1,13 @@
 package simulator;
 
+import Util.Values;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -9,8 +15,14 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Network Simulator");
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("layout.fxml")));
+        Scene scene = new Scene(root, Values.APPLICATION_MIN_WIDTH, Values.APPLICATION_MIN_HEIGHT);
+        stage.setMinWidth(Values.APPLICATION_MIN_WIDTH);
+        stage.setMinHeight(Values.APPLICATION_MIN_HEIGHT);
+
+        stage.setTitle("Network Simulator");
+        stage.setScene(scene);
+        stage.show();
     }
 }
