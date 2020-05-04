7132b491afff260d10c6bbe144f033692d7b2f1d
FuriousSpider <mario123gbm@gmail.com>
2020-05-04 09:33:13 +0200

    add canvas

--- src/main/java/simulator/Controller.java
+++ src/main/java/simulator/Controller.java
@@ -1,4 +1,56 @@
 package simulator;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import util.Values;
+import view.CanvasPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Pane canvasPane;
+    private GraphicsContext ctx;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        CanvasPane canvas = new CanvasPane(canvasPane.getWidth(), canvasPane.getHeight());
+        canvasPane.getChildren().add(canvas);
+        ctx = canvas.getCanvas().getGraphicsContext2D();
+    }
+
+    @FXML
+    private void handleEndDeviceButtonClick(ActionEvent event) {
+        Image image = new Image("/endDevice.png");
+        ctx.drawImage(image, 10, 10, Values.DEVICE_SIZE, Values.DEVICE_SIZE);
+    }
+
+    @FXML
+    private void handleHubButtonClick(ActionEvent event) {
+        Image image = new Image("/hub.png");
+        ctx.drawImage(image, 10, 10, Values.DEVICE_SIZE, Values.DEVICE_SIZE);
+    }
+
+    @FXML
+    private void handleSwitchButtonClick(ActionEvent event) {
+        Image image = new Image("/switch.png");
+        ctx.drawImage(image, 10, 10, Values.DEVICE_SIZE, Values.DEVICE_SIZE);
+    }
+
+    @FXML
+    private void handleRouterButtonClick(ActionEvent event) {
+        Image image = new Image("/router.png");
+        ctx.drawImage(image, 10, 10, Values.DEVICE_SIZE, Values.DEVICE_SIZE);
+    }
+
+    @FXML
+    private void handleFirewallButtonClick(ActionEvent event) {
+        Image image = new Image("/firewall.png");
+        ctx.drawImage(image, 10, 10, Values.DEVICE_SIZE, Values.DEVICE_SIZE);
+    }
 }
--- src/main/java/util/Values.java
+++ src/main/java/util/Values.java
@@ -1,6 +1,8 @@
-package Util;
+package util;
 
 public class Values {
     public static int APPLICATION_MIN_WIDTH = 800;
     public static int APPLICATION_MIN_HEIGHT = 600;
+
+    public static int DEVICE_SIZE = 50;
 }
